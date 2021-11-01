99b5be134804fdaaa80de676e729905ca5bd8e9b
Melissa Keller <melissa.keller23@gmail.com>
2021-11-01 16:46:58 -0500

    added the user log in form and cookie to remember the email

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 
+import re
 from flask import Flask, render_template, request, flash, session, redirect
 from jinja2 import StrictUndefined
 
@@ -7,10 +8,21 @@ app.secret_key = 'picklesaretastey'
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
-def login():
+def homepage():
     """View Homepage"""
 
     return render_template('login.html')
+
+@app.route('/user_login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    first_name = request.form.get('fname')
+    last_name = request.form.get('lname')
+
+    session['email'] = email
+
+    return render_template('coin_entry.html', email = email, first_name = first_name, last_name = last_name)
+
 
 
 if __name__ == "__main__":
